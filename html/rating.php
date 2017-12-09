eefac19fe15c02b82950cd355639c00165641990
M Sabiq Danurrohman <msabiqd@gmail.com>
2017-12-09 23:28:12 +0700

    fix homepage

---
+++ html/rating.php
@@ -0,0 +1,63 @@
+<html>
+	<head>
+		<head>
+		<meta charset="UTF-8">
+		<meta name="description" content="Sentimen Analysis">
+		<meta name="keywords" content="HTML">
+		<meta name="author" content="IR(MA)">
+		<title>Sentiment Analysis Warunk Upnormal</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="./css/indexstyle.css">
+        <link href='https://fonts.googleapis.com/css?family=Karla' rel='stylesheet'>
+	</head>
+	<body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">IR(MA)</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <!-- <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li> -->
+                    <li><a href="../">Home</a></li>
+                    <!-- <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                    </li> -->
+                </ul>
+                <form class="navbar-form navbar-left">
+                    <div class="form-group">
+                        <select class="form-control">
+                            <option value="beji">Warunk Upnormal Beji Depok</option>
+                            <option value="cempaka-putih">Warunk Upnormal Cempaka Putih</option>
+                            <option value="kelapa-gading">Warunk Upnormal Kelapa Gading</option>
+                            <option value="tebet">Warunk Upnormal Tebet</option>
+                            <option value="serpong">Warunk Upnormal Serpong Utara Tangerang</option>
+                        </select>
+                    <!-- <input type="text" class="form-control" placeholder="Search"> -->
+                    </div>
+                    <button type="submit" class="btn btn-success">Cari</button>
+                </form>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+	</body>
+</html>
