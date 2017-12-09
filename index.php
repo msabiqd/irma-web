3ee7d48dab5605bbd2581804ca60c1b8cd91d09f
M Sabiq Danurrohman <msabiqd@gmail.com>
2017-12-09 23:28:12 +0700

    fix homepage

--- index.php
+++ index.php
@@ -6,36 +6,129 @@
 		<meta name="keywords" content="HTML">
 		<meta name="author" content="IR(MA)">
 		<title>Sentiment Analysis Warunk Upnormal</title>
-		<!-- <script type="text/javascript" src="../public_html/ws%20baru/Javascript/jquery.min.js"></script> -->
-		<!-- <link rel="stylesheet" type="text/css" href="../public_html/ws%20baru/bootstrap/css/bootstrap.min.css"> -->
-		<link rel="stylesheet" type="text/css" href="./css/indexstyle.css">
-		<link href='https://fonts.googleapis.com/css?family=Karla' rel='stylesheet'>
-		<!-- <script type="text/javascript" src="../public_html/ws%20baru/bootstrap/js/bootstrap.min.js"></script> -->
-		<!-- <script type="text/javascript" src="../public_html/ws%20baru/Javascript/scriptguestbook.js"></script> -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="./css/indexstyle.css">
+        <link href='https://fonts.googleapis.com/css?family=Karla' rel='stylesheet'>
 	</head>
-	<body>
-		<!-- <img class="backgroundimg" src="./assets/background.jpg" alt="Smiley face"> -->
-		<div class="header">
-			<div class="header-container">
-				<div class"title">
-					<h1> <b>LOREM IPSUM DOLOR SIT AMET </b></h1>
-					<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-						Aenean varius est eget suscipit ultricies. Lorem ipsum dolor sit amet, 
-						consectetur adipiscing elit. Quisque in nisi pellentesque lectus aliquet 
-						interdum. Nulla facilisi. Fusce auctor ante vel ante consequat, at ullamcorper
-						sapien auctor. Etiam aliquet leo non scelerisque aliquet. Vivamus fermentum
-						faucibus dignissim. Quisque eget quam sit amet dui tempor imperdiet.</p>
-				</div>
-				<div class="choose-bar-container">
-					<select class="choose-bar">
-					  <option value="volvo">Cabang1</option>
-					  <option value="saab">Cabang2</option>
-					  <option value="opel">Cabang3</option>
-					  <option value="audi">Cabang4</option>
-					</select>
-					<button class="choose-bar-button">Cari</button>
-				</div>
-			</div>
-		</div>
+	<!-- <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid"> -->
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <!-- <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">IR(MA)</a>
+                </div> -->
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <!-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Link</a></li>
+                    <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                    </li>
+                </ul>
+                <form class="navbar-form navbar-left">
+                    <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Link</a></li>
+                    <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                    </li>
+                </ul> -->
+                <!-- </div>/.navbar-collapse -->
+            <!-- </div>/.container-fluid -->
+        <!-- </nav> -->
+        <div class="header">
+            <div class="header-container">
+                <div class="title">
+                    <h1> <b>IR(MA) AUTO RATING</b></h1>
+                    <p> 
+                        IR(MA) auto rating merupakan sebuah website sentiment analysis terhadap
+                        beberapa cabang warunk upnormal. Dengan menilai komentar dari setiap cabang warunk
+                        upnormal yang berada pada zomato.com, IR(MA) dapat menentukan rating untuk suatu cabang tersebut.
+                    </p>
+                </div>
+                <div class="choose-bar-container">
+                    <div class="form-inline">
+                        <select class="form-control">
+                            <option value="beji">Warunk Upnormal Beji Depok</option>
+                            <option value="cempaka-putih">Warunk Upnormal Cempaka Putih</option>
+                            <option value="kelapa-gading">Warunk Upnormal Kelapa Gading</option>
+                            <option value="tebet">Warunk Upnormal Tebet</option>
+                            <option value="serpong">Warunk Upnormal Serpong Utara Tangerang</option>
+                        </select>
+                        <button type="button" class="btn btn-success">Cari</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="team">
+            <h1 class="team-title"><b> We are . . .</b></h1>
+            <div class="row">
+                <div class="col-sm-4 col-md-3">
+                    <div class="thumbnail">
+                        <img src="assets/ferdinand.jpg" class="img-circle" alt="...">
+                        <div class="caption">
+                            <h3>Ferdinand Chandra</h3>
+                            <p>...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="thumbnail">
+                        <img src="assets/norma.jpg" class="img-circle" alt="...">
+                        <div class="caption">
+                            <h3>Norma Puspitasari</h3>
+                            <p>...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="thumbnail">
+                        <img src="assets/sabiq.jpg" class="img-circle" alt="...">
+                        <div class="caption">
+                            <h3>M Sabiq Danurrohman</h3>
+                            <p>...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="thumbnail">
+                        <img src="assets/hadi.jpg" class="img-circle" alt="...">
+                        <div class="caption">
+                            <h3>Hadi Syah Putra</h3>
+                            <p>...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 	</body>
 </html>
