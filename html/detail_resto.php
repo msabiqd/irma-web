c9a570b8f31b4dd857af4dfa40992c664bf4bce2
M Sabiq Danurrohman <msabiqd@gmail.com>
2017-12-18 15:42:37 +0700

    grafik & user review

--- html/detail_resto.php
+++ html/detail_resto.php
@@ -8,10 +8,21 @@
     <title>Sentiment Analysis Warunk Upnormal</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../css/indexstyle.css">
+    <link rel="stylesheet" type="text/css" href="../css/detail_resto.css">
     <link href='https://fonts.googleapis.com/css?family=Karla' rel='stylesheet'>
 </head>
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="intro">
+                    <h1 class="big-title"></h1>
+                    <h3 class="zomato-resto"></h3>
+                    <h3 class="irma-resto"></h3>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -22,6 +33,27 @@
         </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js"></script>
-    <script src="../js/detail_resto.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/detail_resto.js"></script>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="demo">
+                    <button class="btn btn-primary" onclick="loadDoc()">Lihat Detail</button>
+                    <div class="panel panel-default">
+                        <!-- <div class="panel-body">
+                            asdasd
+                            <p class="rating rating-irma"> <b>rating by irma </b><p>
+                            <p class="rating rating-zomato"> <b>rating by zomato </b><p>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+    </div>
+    
+    
 </body>
 </html>
--- js/detail_resto.js
+++ js/detail_resto.js
@@ -1,6 +1,90 @@
 var randomScalingFactor = function() {
     return Math.round(Math.random() * 100);
 };
+var increment = 0;
+var beji_data = function(){
+    var tmp = null;
+
+    $.ajax({
+        url: "http://www.mocky.io/v2/5a34abbd3000004b324b7640",
+        type: "GET",
+        async: false, 
+        contentType: 'application/json; charset=utf-8',
+        success: function(resultData) {
+            tmp = resultData;
+            console.log(tmp);
+            console.log("resData" + resultData)
+        },
+        error : function(jqXHR, textStatus, errorThrown) {
+        },
+    
+        timeout: 120000,
+    }).responseText ;
+    return tmp;
+}();
+
+
+function loadDoc() {
+    var result = '<h1 class="title-review">User review</h1>';
+    increment += 5;
+    var max;
+    if (increment > beji_data.reviews.length) {
+        max = beji_data.reviews.length;
+    } else {
+        max = increment;
+    }
+    for (i = 0; i < max; i++) {
+        result += '<div class="panel panel-default"><div class="panel-body">' + 
+                  beji_data.reviews[i].review_body + window.location.href.split("=")[1] +'<p class="rating rating-irma"> <b>rating by irma : ' + beji_data.reviews[i].rating_irma + '</b><p>'
+                  + '<p class="rating rating-zomato"> <b>rating by zomato : ' + beji_data.reviews[i].rating_zomato + '</b><p>' +
+                  '</div></div>'
+    }
+    // document.getElementById("demo").innerHTML = beji_data.reviews[0].rating_zomato;
+    // $('.btn btn-primary').remove();
+    result += '<button type="button" class="btn btn-primary" onclick="loadMore()">Load More</button>'
+    var div = $('#demo');
+    // div.slideUp(1); 
+    div.html(result);
+    // div.fadeIn(1000);  
+    $('html, body').animate({
+        scrollTop: $(".title-review").offset().top
+    }, 1000);
+}
+
+function loadMore() {
+    $('.btn-primary').remove();
+    var result = ""
+    var start = increment;
+    increment += 5;
+    var max;
+    if (increment > beji_data.reviews.length) {
+        max = beji_data.reviews.length;
+    } else {
+        max = increment;
+    }
+    for (i = start; i < max; i++) {
+        result += '<div class="panel panel-default"><div class="panel-body">' + 
+                  beji_data.reviews[i].review_body + '<p class="rating rating-irma"> <b>rating by irma : ' + beji_data.reviews[i].rating_irma + '</b></p>'
+                  + '<p class="rating rating-zomato"> <b>rating by zomato : ' + beji_data.reviews[i].rating_zomato + '</b></p>' +
+                  '</div></div>' 
+    }
+    if (increment < beji_data.reviews.length) {
+        result += '<button type="button" class="btn btn-primary" onclick="loadMore()">Load More</button>'
+    }
+    // var div = $("#demo");
+    // div.slideUp(1);
+    $("#demo").append(result);
+    // div.fadeIn(1000);  
+    // $('html, body').animate({
+    //     scrollTop: $(".footer").offset().top+1000
+    // }, 2000);
+     
+    // div.html(result);
+    // div.fadeIn(1000);  
+    $('html, body').animate({
+        scrollTop: $(".btn-primary").offset().top
+    }, 1000);
+}
 
 var config = {
     type: 'line',
@@ -11,13 +95,26 @@ var config = {
             fill: false,
             backgroundColor: 'red',
             borderColor: 'red',
-            data: [2.0,3.0,2.0,3.0,1.5,1.0,1.0,3.0,3.0,3.5,1.0,3.5,2.5,4.0,3.5,4.5,4.0,2.5,2.5,4.0,3.5,2.0,1.0,2.5,1.0]
+        //    data: [beji_data['rating_zomato']]
+            data: [beji_data.reviews[0].rating_zomato,beji_data.reviews[1].rating_zomato,beji_data.reviews[2].rating_zomato,
+            beji_data.reviews[3].rating_zomato,beji_data.reviews[4].rating_zomato,beji_data.reviews[5].rating_zomato,beji_data.reviews[6].rating_zomato,
+            beji_data.reviews[7].rating_zomato,beji_data.reviews[8].rating_zomato,beji_data.reviews[9].rating_zomato,beji_data.reviews[10].rating_zomato,
+            beji_data.reviews[11].rating_zomato,beji_data.reviews[12].rating_zomato,beji_data.reviews[13].rating_zomato,beji_data.reviews[14].rating_zomato,
+            beji_data.reviews[15].rating_zomato,beji_data.reviews[16].rating_zomato,beji_data.reviews[17].rating_zomato,beji_data.reviews[18].rating_zomato,
+            beji_data.reviews[19].rating_zomato,beji_data.reviews[20].rating_zomato,beji_data.reviews[21].rating_zomato,beji_data.reviews[22].rating_zomato,
+            beji_data.reviews[23].rating_zomato,beji_data.reviews[24].rating_zomato,beji_data.reviews[25].rating_zomato,]
         }, {
             label: "IR(MA) review",
             fill: false,
             backgroundColor: 'blue',
             borderColor: 'blue',
-            data: [2.0,2.2,2.066666666666667,2.3333333333333335,2.0,2.0,2.0,1.6666666666666667,2.0,3.0,2.0,2.2,2.0,1.8333333333333333,2.0,2.0,2.1666666666666665,2.0,2.0,2.2,2.0,2.0,2.0,2.0,2.0],
+            data: [beji_data.reviews[0].rating_irma,beji_data.reviews[1].rating_irma,beji_data.reviews[2].rating_irma,
+            beji_data.reviews[3].rating_irma,beji_data.reviews[4].rating_irma,beji_data.reviews[5].rating_irma,beji_data.reviews[6].rating_irma,
+            beji_data.reviews[7].rating_irma,beji_data.reviews[8].rating_irma,beji_data.reviews[9].rating_irma,beji_data.reviews[10].rating_irma,
+            beji_data.reviews[11].rating_irma,beji_data.reviews[12].rating_irma,beji_data.reviews[13].rating_irma,beji_data.reviews[14].rating_irma,
+            beji_data.reviews[15].rating_irma,beji_data.reviews[16].rating_irma,beji_data.reviews[17].rating_irma,beji_data.reviews[18].rating_irma,
+            beji_data.reviews[19].rating_irma,beji_data.reviews[20].rating_irma,beji_data.reviews[21].rating_irma,beji_data.reviews[22].rating_irma,
+            beji_data.reviews[23].rating_irma,beji_data.reviews[24].rating_irma,beji_data.reviews[25].rating_irma,],
         }]
     },
     options: {
@@ -60,9 +157,36 @@ function findGetParameter(parameterName) {
     return result;
 }
 
+
+// function loadDoc() {
+//     $.ajax({
+//         url: "http://www.mocky.io/v2/5a34abbd3000004b324b7640",
+//         type: "GET",
+    
+//         contentType: 'application/json; charset=utf-8',
+//         success: function(resultData) {
+//             //here is your json.
+//               // process it
+//               var result = JSON.stringify(resultData)
+//               document.getElementById("demo").innerHTML = resultData['rating_zomato']
+//         },
+//         error : function(jqXHR, textStatus, errorThrown) {
+//         },
+    
+//         timeout: 120000,
+//     });
+// }
+
 window.onload = function() {
     var param = findGetParameter('resto_option')
     var ctx = document.getElementById("lineChart").getContext("2d");
+    $('.big-title').html('Warunk Upnormal Cabang ' + window.location.href.split("=")[1] + '<br><br>');
+    $('.zomato-resto').html('Zomato Rating : ' + beji_data.rating_zomato);
+    $('.irma-resto').html('IR(MA) Rating : ' + beji_data.rating_irma);
     window.myLine = new Chart(ctx, config);
 };
+
+
+
+
   
